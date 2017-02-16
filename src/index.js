2c60867755de825fe4af6dd179faddebd3a345c3
Andy Mention <amention@gmail.com>
2017-02-16 13:37:34 -0700

    Add SearchBar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAUza3dZBjjcyXwb8590IstPQHMcup0ZFU';
+
 const App = () => {
-  return <div> Hello World! </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
